8630674d712435d0127ac62308b06e4f9493b401
Veronika Kurinna <kurinna.dev@gmail.com>
2024-04-18 20:57:36 +0300

    Add function getData

---
+++ ToDoList/wwwroot/js/toDoList.js
@@ -0,0 +1,33 @@
+﻿getData();
+
+function addListItems(data) {
+    const ul = document.querySelector("ul");
+
+    for (var i = 0; i < data.length; i++) {
+        let li = document.createElement("li");
+        li.classList.add("list-group-item");
+        li.innerHTML = `<input class="form-check-input me-1" type="checkbox" value="${data[i].id}" id="${data[i].id}">
+                        <label for="${data[i].id}">${data[i].name}</label>`;
+        ul.append(li);
+    }
+}
+
+function getData() {
+    let url = "http://localhost:5226/api/ToDoListItem/Get";
+
+    let promise = fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    promise
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            addListItems(data.toDoListItems);
+        })
+    .catch(error => console.log(error.message));
+}
