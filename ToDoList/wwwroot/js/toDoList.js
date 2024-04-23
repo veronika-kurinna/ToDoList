f2aa5727b3bec20685da2395d768c4ed34fa53e4
Veronika Kurinna <kurinna.dev@gmail.com>
2024-04-23 22:49:38 +0300

    chore: create httpClient.js

--- ToDoList/wwwroot/js/toDoList.js
+++ ToDoList/wwwroot/js/toDoList.js
@@ -7,44 +7,6 @@ function renderToDoList(items) {
     }
 }
 
-function getToDoListItems() {
-    let url = "http://localhost:5226/api/ToDoListItem/Get";
-    let request = {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    };
-
-    return fetch(url, request)
-        .then(response => response.json())
-        .then(response => response.toDoListItems)
-        .catch(error => console.log(error.message));
-}
-
-function createToDoListItem() {
-    let url = "http://localhost:5226/api/ToDoListItem/Create";
-    let toDoListItem = document.querySelector("#newItem").value;
-
-    if (!toDoListItem) {
-        throw new Error("Item is required");
-    }
-
-    let request = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            name: toDoListItem
-        })
-    };
-
-    return fetch(url, request)
-        .then(response => response.json())
-        .catch(error => console.log(error.message));
-}
-
 function renderItem(item) {
     let ul = document.querySelector("ul");
     let li = document.createElement("li");
