733bd091ed9e8172a94095f52c72420342f1fe4f
Veronika Kurinna <kurinna.dev@gmail.com>
2024-05-10 20:30:54 +0300

    feat: add new buttons, input

--- ToDoList/wwwroot/js/toDoList.js
+++ ToDoList/wwwroot/js/toDoList.js
@@ -17,17 +17,8 @@ function renderItem(item) {
     let li = document.createElement("li");
 
     li.classList.add("list-group-item");
-    li.innerHTML = `<div class="toDoListItem">
-                        <div>
-                            <input class="form-check-input me-1" type="checkbox" id="${item.id}" onclick='toggleStatusClickHandler(${item.id})' ${item.status == statusDone ? 'checked' : ''}>
-                            <label class="label-${item.id} ${item.status == statusDone ? 'strikethrough' : ''}">${item.name}</label>
-                        </div>
-                        <div class="btn-group me-1">
-                            <button class="btn btn-outline-secondary" type="button" id="" onclick='toggleStatusArchivedClickHandler(${item.id})'>
-                                <i class="bi-${item.id} ${item.status == statusArchived ? 'bi-arrow-down-square-fill' : 'bi-arrow-up-square-fill'}"></i>
-                            </button>
-                        </div>
-                    </div>`;
+    li.classList.add(item.id);
+    li.innerHTML = returnItem(item);
     ul.append(li);
 }
 
@@ -82,4 +73,55 @@ function toggleStatusArchivedClickHandler(id) {
         item.status = statusArchived;
         updateToDoListItem(item);
     }
+} 
+
+function returnItem(item) {
+    return `<div class="toDoListItem" id="${item.id}">
+                <div>
+                    <input class="form-check-input me-1" type="checkbox" onclick='toggleStatusClickHandler(${item.id})' ${item.status == statusDone ? 'checked' : ''}>
+                    <label class="label-${item.id} ${item.status == statusDone ? 'strikethrough' : ''}">${item.name}</label>
+                </div>
+                <div class="btn-group me-1">
+                    <button class="btn btn-outline-secondary" type="button" onclick='editNameClickHandler(${item.id})'>
+	                    <i class="bi bi-pencil-fill"></i>
+                    </button>
+                    <button class="btn btn-outline-secondary" type="button" onclick='toggleStatusArchivedClickHandler(${item.id})'>
+                        <i class="bi-${item.id} ${item.status == statusArchived ? 'bi-arrow-down-square-fill' : 'bi-arrow-up-square-fill'}"></i>
+                    </button>
+                </div>
+            </div>`;
+}
+
+function editNameClickHandler(id) {
+    let item = toDoListItems.find(e => e.id == id);
+    let classToDoListItem = document.getElementById(item.id);
+    classToDoListItem.remove();
+
+    let li = document.getElementsByClassName(`list-group-item ${item.id}`)[0];
+    let div = document.createElement("div");
+    div.classList.add("input-group");
+    div.classList.add(item.id);
+    div.innerHTML = `<input type="text" class="form-control" id="editItemInput" value="${item.name}">
+                     <button class="btn btn-primary" type="button" onclick='saveNameClickHandler(${item.id})'>Save</button>`;
+    li.append(div);
+}
+
+function saveNameClickHandler(id) {
+    let item = toDoListItems.find(e => e.id == id);
+    let input = document.querySelector("#editItemInput");
+    let editedName = input.value;
+
+    if (!editedName) {
+        throw new Error("Item is required");
+    }
+
+    item.name = editedName;
+    updateToDoListItem(item);
+
+    let inp = document.getElementsByClassName(`input-group ${item.id}`)[0];
+    inp.remove();
+
+    let li = document.getElementsByClassName(`list-group-item ${item.id}`)[0];
+    li.innerHTML = returnItem(item);
+    li.append();
 }
